ef5503e1e1ead41fc61b41db15b90973cd75de3b
Eric Zhu <zhueric27@gmail.com>
2019-09-03 11:06:11 -0400

    First Commit

---
+++ cmake-build-debug/CMakeFiles/movielist.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/movielist.dir/main.c.o"
+  "movielist.pdb"
+  "movielist"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/movielist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/movielist.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for movielist.
+# This may be replaced when dependencies are built.
